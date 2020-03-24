6407ef5f9ca86df0150c110e7ff61a91c305cbd6
Jan Wolski <jan@wolski.fi>
2020-03-24 12:14:47 +0200

    Updates

--- slider-helper.js
+++
@@ -1,5 +0,0 @@
-export function onRangeChange(r, f) {
-    var n, c, m;
-    r.addEventListener("input", function (e) { n = 1; c = e.target.value; if (c != m) f(e); m = c; });
-    r.addEventListener("change", function (e) { if (!n) f(e); });
-}
